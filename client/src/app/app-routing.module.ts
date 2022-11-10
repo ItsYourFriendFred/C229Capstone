926a41906f6c4a7484ca31e6d5336a9d1e45b7a1
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-11-10 02:43:09 -0500

    Renamed partials to components, removed TS .spec emits & changed emits to same directory, added components for login/register, reorganized folder structure

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,16 +4,21 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { FaqComponent } from './pages/faq/faq.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SurveyDetailsComponent } from './survey/survey-details/survey-create.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SurveyDetailsComponent } from './survey/survey-details/survey-details.component';
 import { SurveyListComponent } from './survey/survey-list/survey-list.component';
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent, data: {title: ""}},
-  {path: "about", component: AboutComponent, data: {title: ""}},
-  {path: "contact", component: ContactComponent, data: {title: ""}},
-  {path: "faq", component: FaqComponent, data: {title: ""}},
-  {path: "survey-list", component: SurveyListComponent, data: {title: ""}},
-  {path: "survey-details", component: SurveyDetailsComponent, data: {title: ""}},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent, data: {title: ''}},
+  {path: 'about', component: AboutComponent, data: {title: ''}},
+  {path: 'contact', component: ContactComponent, data: {title: ''}},
+  {path: 'faq', component: FaqComponent, data: {title: ''}},
+  {path: 'survey-list', component: SurveyListComponent, data: {title: ''}},
+  {path: 'survey-details', component: SurveyDetailsComponent, data: {title: ''}},
+  {path: 'login', component: LoginComponent, data: {title: ''}},
+  {path: 'register', component: RegisterComponent, data: {title: ''}}
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,29 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './partials/navbar/navbar.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { FaqComponent } from './pages/faq/faq.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SurveyDetailsComponent } from './survey/survey-details/survey-details.component';
 import { SurveyListComponent } from './survey/survey-list/survey-list.component';
-import { SurveyDetailsComponent } from './survey/survey-details/survey-create.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
-    HomeComponent,
     AboutComponent,
-    FaqComponent,
     ContactComponent,
-    SurveyListComponent,
+    HomeComponent,
+    FaqComponent,
+    FooterComponent,
+    NavbarComponent,
     SurveyDetailsComponent,
+    SurveyListComponent,
+    LoginComponent,
+    RegisterComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
