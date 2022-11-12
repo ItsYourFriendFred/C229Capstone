31ff2c34afbc313ae4d9bab95d6d21b1136ae69c
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2022-11-12 15:34:11 -0500

    work on survey model + create submodel modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,35 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { HomeComponent } from './pages/home/home.component';
-import { FaqComponent } from './pages/faq/faq.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SurveyDetailsComponent } from './survey/survey-details/survey-details.component';
-import { SurveyListComponent } from './survey/survey-list/survey-list.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
+import { PartialsModule } from './partials/components.module';
+import { SurveyModule } from './survey/survey.module';
+import { UserModule } from './user/user.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    ContactComponent,
-    HomeComponent,
-    FaqComponent,
-    FooterComponent,
-    NavbarComponent,
-    SurveyDetailsComponent,
-    SurveyListComponent,
-    LoginComponent,
-    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    PartialsModule,
+    SurveyModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
