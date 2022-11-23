7a123f1867829156d328a86bb86b54a70751de4c
Fred Wong <112927300+ItsYourFriendFred@users.noreply.github.com>
2022-11-22 19:26:29 -0500

    Made more clear about dotenv usage in server.ts

--- back-end/server.ts
+++ back-end/server.ts
@@ -7,22 +7,25 @@ Server: Initialization of middleware, setting up routes
 /**
  * Module dependencies.
  */
-require("dotenv").config();
-import app from "./server/config/app";
+
+/* Configure server to load environment variables from a .env file*/
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import app from './server/config/app';
 //const debug = require('debug')('back-end:server');
-import debug from "debug";
-debug("back-end:server");
-import http from "http";
-import { HttpError } from "http-errors";
-import { AddressInfo } from "net";
-import { use } from "passport";
+import debug from 'debug';
+debug('back-end:server');
+import http from 'http';
+import { HttpError } from 'http-errors';
+import { AddressInfo } from 'net';
 
 /**
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || "3500");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3500');
+app.set('port', port);
 
 /**
  * Create HTTP server.
@@ -35,8 +38,8 @@ const server = http.createServer(app);
  */
 
 server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -63,20 +66,22 @@ function normalizePort(val: string) {
  */
 
 function onError(error: HttpError) {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
-  let bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
       break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
       break;
     default:
@@ -90,11 +95,13 @@ function onError(error: HttpError) {
 
 function onListening() {
   let addr = server.address() as string | AddressInfo;
-  let bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  let bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
 
   /*  
   let addr = server.address() as string;
   let bind = 'pipe ' + addr;
   */
-  debug("Listening on " + bind);
+  debug('Listening on ' + bind);
 }
