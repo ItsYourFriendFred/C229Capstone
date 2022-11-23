08b2ac6d0f36c456914b17caafa75e76b89fb43f
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2022-11-23 11:27:09 -0500

    adapt edit survey page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,6 +18,7 @@ const routes: Routes = [
   {path: 'faq', component: FaqComponent, data: {title: ''}},
   {path: 'survey-list', component: SurveyListComponent, data: {title: ''}},
   {path: 'survey-details', component: SurveyDetailsComponent, data: {title: ''}},
+  {path: 'edit/:id', component: SurveyDetailsComponent, data: {title: ''}},
   {path: 'login', component: LoginComponent, data: {title: ''}},
   {path: 'register', component: RegisterComponent, data: {title: ''}},
   {path: 'new-survey', component: NewSurveyComponent, data: {title: ''}},
--- client/src/app/survey/survey-details/survey-details.component.ts
+++ client/src/app/survey/survey-details/survey-details.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Survey } from 'src/app/model/survey.model';
+import { SurveyRepository } from 'src/app/model/survey.repository';
+import { SurveyModule } from '../survey.module';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { param } from 'jquery';
 @Component({
   selector: 'app-survey-details',
   templateUrl: './survey-details.component.html',
-  styleUrls: ['./survey-details.component.css']
+  styleUrls: ['./survey-details.component.css'],
 })
 export class SurveyDetailsComponent implements OnInit {
+  private surveyID: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private repository: SurveyRepository,
+    private route: ActivatedRoute
+  ) {
+    this.surveyID = '';
   }
 
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.surveyID = params['id'];
+    });
+  }
+
+  get survey(): Survey {
+    console.log(this.surveyID);
+    return this.repository.getSurvey(this.surveyID);
+  }
 }
