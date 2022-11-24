1cd78a2180682f6411d22d875abcee0a8ca58a04
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2022-11-24 17:47:10 -0500

    edit survey adapted to form control

--- client/src/app/survey/survey-details/survey-details.component.ts
+++ client/src/app/survey/survey-details/survey-details.component.ts
@@ -1,46 +1,133 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, NgModule, OnInit } from '@angular/core';
 import { Survey } from 'src/app/model/survey.model';
 import { SurveyRepository } from 'src/app/model/survey.repository';
 import { SurveyModule } from '../survey.module';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { param } from 'jquery';
+import { Subscription, VirtualTimeScheduler } from 'rxjs';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Event, Params, Router } from '@angular/router';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+  Form,
+} from '@angular/forms';
 @Component({
   selector: 'app-survey-details',
   templateUrl: './survey-details.component.html',
   styleUrls: ['./survey-details.component.css'],
 })
 export class SurveyDetailsComponent implements OnInit {
-  private surveyID?: string;
-  private surveyToEdit?: Survey;
+  surveyID!: string;
+  surveyForm!: FormGroup;
+  survey!: Survey;
 
   constructor(
     private repository: SurveyRepository,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {}
-
-  get survey(): Survey {
-    this.route.params.subscribe((params) => {
-      this.surveyID = params['id'];
-      this.surveyToEdit = this.repository.getSurvey(this.surveyID!);
+  ngOnInit(): void {
+    this.surveyID = this.route.snapshot.params['id'];
+    this.route.params.subscribe((param) => {
+      this.readFromParam(param['id']);
+      
+      if (this.router.getCurrentNavigation()?.trigger === 'popstate') {
+        if (Object.keys(param).length !== 0) {
+          this.initialisePage();
+        }
+      }
+      this.initialisePage();
     });
-    return this.surveyToEdit!;
   }
 
-  addQuestion(): void {
-    this.surveyToEdit!.questionsBloc!.push({ question: '', options: [''] });
+  readFromParam(param: string) {
+    this.surveyID = param;
+    this.survey = this.repository.getSurvey(this.surveyID!);
   }
 
-  addOption(i: number): void {
-    this.surveyToEdit!.questionsBloc![i].options!.push('');
+  initialisePage() {
+    this.surveyForm = new FormGroup({
+      title: new FormControl(this.survey.title ? this.survey.title! : ''),
+      type: new FormControl(this.survey.type ? this.survey.type! : ''),
+      dateStart: new FormControl(
+        new Date(this.survey.dateStart ? this.survey.dateStart! : '')
+          .toISOString()
+          .split('T')[0]
+      ),
+      dateEnd: new FormControl(
+        new Date(this.survey.dateEnd ? this.survey.dateEnd! : '')
+          .toISOString()
+          .split('T')[0]
+      ),
+      questionsBloc: new FormArray([]),
+    });
+
+    for (let i = 0; i < this.survey.questionsBloc!.length; i++) {
+      this.addQuestion(this.survey.questionsBloc![i].question);
+      this.survey.questionsBloc![i].options?.forEach((option) => {
+        this.addOption(i, option);
+      });
+    }
+    if (this.survey._id) {
+      sessionStorage.setItem(
+        this.survey._id!,
+        JSON.stringify(this.surveyForm.value)
+      );
+    }
   }
 
-  deleteQuestion(i: number): void {
-    this.surveyToEdit!.questionsBloc!.splice(i, 1);
+  initQuestion(question: string = '') {
+    return new FormGroup({
+      question: new FormControl(question),
+      options: new FormArray([]),
+    });
   }
-  deleteOption(i: number, j: number): void {
-    this.surveyToEdit!.questionsBloc![i].options!.splice(j, 1);
+
+  initOption(option: string = '') {
+    return new FormGroup({
+      option: new FormControl(option),
+    });
+  }
+
+  addQuestion(question: string = '') {
+    const control = <FormArray>this.surveyForm.get('questionsBloc');
+    control.push(this.initQuestion(question));
+  }
+
+  addOption(j: number, option: string = '') {
+    console.log(j);
+    const control = <FormArray>(
+      this.surveyForm.get(['questionsBloc', j, 'options'])
+    );
+    control.push(this.initOption(option));
+  }
+
+  getQuestions(form: any) {
+    return form.controls.questionsBloc.controls;
+  }
+
+  getOptions(form: any) {
+    return form.controls.options.controls;
+  }
+
+  removeQuestion(i: number) {
+    const control = <FormArray>this.surveyForm.get('questionsBloc');
+    control.removeAt(i);
+  }
+
+  removeOption(i: number, j: number) {
+    console.log(j);
+    const control = <FormArray>(
+      this.surveyForm.get(['questionsBloc', i, 'options'])
+    );
+    control.removeAt(j);
+  }
+
+  onSubmit(form: any) {
+    console.log(form.value);
   }
 }
